53fa9a1e3cfe8f50b80044717e9088d58f5c11a9
Krystof2so <christo.2so@gmail.com>
2024-10-11 13:02:40 +0200

    optimize(projects) - open project
    modify 'manage_project.lua', functions 'open_project() and close_project()

--- lua/config/features/project/manage_project.lua
+++ lua/config/features/project/manage_project.lua
@@ -7,91 +7,131 @@
 
 
 local display_msg = require "config.features.display_msg"
+local projects_file = vim.fn.expand("~/.config/nvim/lua/config/features/project/projects.lua")
+local cmd = vim.cmd()
 
 local M = {}
 
 
-function M.open_project() -- Pour l'ouverture d'un projet avec Telescope
+-- ******************************************************************************************************
+-- * Fonction 'update_projects_file' :
+-- * Fonction locale pour ouvrir et écrire dans le fichier 'projects.lua'.                              *
+-- * Cette fonction reçoit la table projects (liste des projets) et met à jour le fichier projects.lua. *
+-- * Centralise toute la logique d'écriture dans le fihcier.                                            *
+-- ******************************************************************************************************
+local function update_projects_file(projects) -- projects = table qui représente la liste des projets
+	-- Ouverture du fichier en mode écriture :
+  local file_projects, err = io.open(projects_file, "w") -- Ecriture si n'existe pas, ou écrasement contenu
+  if not file_projects then -- si le fichier ne peut être ouvert (raison quelconque)
+    display_msg.print_message("Erreur lors de l'écriture de projects.lua : " .. err)
+    return false
+  end
+
+	-- Ecriture dans le fihcier :
+  file_projects:write("-- Liste des projets\n") -- Commentaire d'en-tête
+  file_projects:write("local M = {}\n\n") -- Création d'une table vide
+  file_projects:write("M.projects = {\n") -- Création d'une table 'projects' vide
+
+  -- Parcourt de la liste des projets :
+  for _, project in ipairs(projects) do -- ipairs pour parcourir dans l'ordre
+    if project.name and project.path then -- Vérification des champs pour chaque projet
+      file_projects:write(string.format("  { name = '%s', path = '%s' },\n", project.name, project.path)) -- écriture projet
+    else -- gestion erreur (si un des champs est manquant = projet ignoré)
+      display_msg.print_message("Projet invalide : nom ou chemin manquant.")
+    end
+  end
+
+	-- fermeture de la table des projets et du fichier
+  file_projects:write("}\n\n") -- pour fermer la table 'projects'
+  file_projects:write("return M\n") -- pour charge le fichier 'projects.lua' en tant que module par depuis d'autres fichiers
+  file_projects:close() -- Fermeture et enregistrement
+
+  return true  -- Si tout est OK
+end
+
+
+-- ***********************************************************************************************************
+-- * Fonction 'remove_invalid_project' :                                                                     *
+-- * Fonction qui a pour but de supprimer un projet invalide dans la liste des projets,                      *
+-- * en comparant le chemin du projet (dans la table project.path) avec le chemin spécifié par project_path. *
+-- * Arguments :                                                                                             *
+-- *     - projects : table qui contient la liste des projets.                                               *
+-- *     - project_path : chaîne de caractères représentant le chemin du projet à supprimer.                 *                 
+-- ***********************************************************************************************************
+local function remove_invalid_project(projects, project_path)
+  for i, project in ipairs(projects) do -- itération sur a liste des projets via l'indice 'i'
+    if project.path == project_path then -- Vérification
+      table.remove(projects, i) -- Suppression
+      return true
+    end
+  end
+  return false  -- Rien a été supprimé
+end
+
+
+-- **********************************************************************************************
+-- * Fonction 'open_project' :                                                                  *
+-- * Implémente une fonctionnalité dans Neovim pour sélectionner, vérifier et ouvrir un projet, *
+-- * à partir d'une liste via l'interface de Telescope.                                         *
+-- * cf. https://github.com/nvim-telescope/telescope.nvim/blob/master/doc/telescope.txt         *
+-- * Gestion de la logique d'ouverture d'n projet.                                              *
+-- * Utilisation de Telescope et nvim-tree                                                      *
+-- **********************************************************************************************
+function M.open_project()
+	-- Chargement des modules indispensables au fonctionnement de Telescope, et des dépendances locales :
   local pickers = require "telescope.pickers"
   local finders = require "telescope.finders"
   local conf = require("telescope.config").values
   local actions = require "telescope.actions"
   local action_state = require "telescope.actions.state"
-  local projects = require "config.features.project.projects" -- Appel du fichier contenant la liste des projets
+  local projects = require "config.features.project.projects" -- Liste des projets
 
-  pickers.new({}, {
-    prompt_title = "Sélectionner un projet",
-    finder = finders.new_table {
-      results = projects.projects,
-      entry_maker = function(entry)
+  pickers.new({}, { -- Interface de sélection du projet
+    prompt_title = "Sélectionner un projet à ouvrir", -- Titre de la fenêtre Telescope
+    finder = finders.new_table { -- Récupération de la liste des projets
+      results = projects.projects, -- Liste des projets
+      entry_maker = function(entry) -- Formatage de l'affichage du projet
         return {
           value = entry,
-          display = entry.name,
-          ordinal = entry.name,
+          display = entry.name, -- nom projet
+          ordinal = entry.name, -- indexation
         }
       end
     },
-    sorter = conf.generic_sorter({}),
-    attach_mappings = function(prompt_bufnr)
-      actions.select_default:replace(function()
-        local selection = action_state.get_selected_entry()
-        local project_path = selection.value.path
 
-        -- Vérifie si le répertoire existe
-        if vim.fn.isdirectory(project_path) == 0 then
-          -- Si le répertoire n'existe pas, supprimer le projet de la liste
-          for i, project in ipairs(projects.projects) do
-            if project.path == project_path then
-              table.remove(projects.projects, i)
-              break
-            end
+    sorter = conf.generic_sorter({}), -- Tri/classement des projets
+    attach_mappings = function(prompt_bufnr) -- Actions attachées à l'interface
+      actions.select_default:replace(function() -- Sélection par défaut
+        local selection = action_state.get_selected_entry() -- Récupère l'élément sélectionné
+        local project_path = selection.value.path -- Récupération du chemin du projet
+
+        -- Vérifie existance du projet :
+        if not vim.fn.isdirectory(project_path) then -- Si chemin existe dans le sstème de fihciers
+          -- Supprime le projet si le répertoire n'existe pas :
+          if remove_invalid_project(projects.projects, project_path) then
+            update_projects_file(projects.projects)
+            display_msg.print_message("Projet inexistant et supprimé de la liste.")
+          else
+            display_msg.print_message("Erreur : impossible de trouver le projet.")
           end
-
-          -- Mettre à jour le fichier projects.lua après suppression
-          local projects_file = "~/.config/nvim/lua/config/features/project/projects.lua"
-          local file_projects = io.open(vim.fn.expand(projects_file), "w")
-					-- Vérifie si le fichier est ouvert correctement
-					if not file_projects then
-            display_msg.print_message("Erreur : impossible d'ouvrir le fichier projects.lua pour l'écriture.")
-            return
-          end
-
-          file_projects:write("-- Liste des projets\n")
-          file_projects:write("local M = {}\n\n")
-          file_projects:write("M.projects = {\n")
-
-					-- Parcourt la liste des projets, tout en vérifiant `project.name` et `project.path`
-					for _, project in ipairs(projects.projects) do
-						if project.name and project.path then
-							file_projects:write("  { name = '" .. project.name .. "', path = '" .. project.path .. "' },\n")
-						else
-							display_msg.print_message("Erreur : le projet est invalide (nom ou chemin manquant).")
-						end
-          end
-
-          file_projects:write("}\n\n")
-          file_projects:write("return M\n")
-          file_projects:close()
-
-          actions.close(prompt_bufnr)
-          display_msg.print_message("Projet inexistant et supprimé de la liste.")
+          actions.close(prompt_bufnr) -- Fermeture Telescope
           return
         end
 
-        -- Si le répertoire existe, changer de répertoire et ouvrir NvimTree
+        -- Si le projet existe, ouvrir l'arborescence des fichiers du projet avec NvimTree :
         actions.close(prompt_bufnr)
-        vim.cmd("cd " .. project_path)
-        vim.cmd("NvimTreeOpen")
+        cmd("cd " .. project_path) -- Change le répertoire courant
+        cmd("NvimTreeOpen") -- Ouvre NvimTree 
       end)
       return true
     end,
-  }):find()
+  }):find() -- Affichage du picker
 end
 
 
 function M.close_project() -- Pour la fermeture d'un projet
-	vim.cmd("wall") -- Sauvegarder tous les fichiers modifiés
-  vim.cmd("qall!") -- Quitter Neovim en forçant la fermeture de tous les buffers
+	cmd("wall") -- Sauvegarder tous les fichiers modifiés
+  cmd("qall!") -- Quitter Neovim en forçant la fermeture de tous les buffers
 end
 
 
